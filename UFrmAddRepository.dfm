502c4855c75da0d4acb785de69f25aa6f1dd99ed
knst.spb@gmail.com <superbot-coder>
2022-04-04 05:44:57 +0800

    04.04.2022

---
+++ UFrmAddRepository.dfm
@@ -0,0 +1,205 @@
+object FrmAddRepository: TFrmAddRepository
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
+  Caption = #1044#1086#1073#1072#1074#1080#1090#1100' '#1085#1086#1074#1099#1081' '#1087#1088#1086#1077#1082#1090
+  ClientHeight = 551
+  ClientWidth = 587
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  KeyPreview = True
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  OnKeyDown = FormKeyDown
+  PixelsPerInch = 96
+  TextHeight = 13
+  object LblUrlProject: TLabel
+    Left = 16
+    Top = 16
+    Width = 140
+    Height = 13
+    Caption = 'C'#1089#1099#1083#1082#1072' '#1087#1088#1086#1077#1082#1090#1072' '#1085#1072' GitHub: '
+  end
+  object SpdBtnOpenDir: TSpeedButton
+    Left = 440
+    Top = 104
+    Width = 23
+    Height = 22
+    Caption = '...'
+    OnClick = SpdBtnOpenDirClick
+  end
+  object edRepositoryLink: TEdit
+    Left = 16
+    Top = 32
+    Width = 441
+    Height = 21
+    MaxLength = 1000
+    TabOrder = 0
+    Text = 'https://github.com/superbot-coder/chia_plotting_tools'
+    OnChange = edRepositoryLinkChange
+  end
+  object statTextProjectDir: TStaticText
+    Left = 16
+    Top = 72
+    Width = 441
+    Height = 25
+    AutoSize = False
+    Caption = 
+      #1044#1080#1088#1077#1082#1090#1086#1088#1080#1103' '#1076#1083#1103' '#1089#1082#1072#1095#1080#1074#1072#1085#1080#1103' '#1088#1077#1083#1080#1079#1086#1074' '#1101#1090#1086#1075#1086' '#1087#1088#1086#1077#1082#1090#1072': '#13#10'('#1087#1091#1090#1100' '#1087#1086' '#1091#1084#1086#1083 +
+      #1095#1072#1085#1080#1102' <User profile>\Downloads\GitHubReleasesTracker\<project na' +
+      'me>)'
+    TabOrder = 1
+  end
+  object ChBoxSubDir: TCheckBox
+    Left = 16
+    Top = 144
+    Width = 209
+    Height = 17
+    Caption = #1044#1083#1103' '#1082#1072#1078#1076#1086#1075#1086' '#1088#1077#1083#1080#1079#1072' '#1089#1091#1073#1076#1080#1088#1077#1082#1090#1086#1088#1080#1103
+    Checked = True
+    State = cbChecked
+    TabOrder = 2
+  end
+  object ChBoxDownloadLastRelease: TCheckBox
+    Left = 16
+    Top = 192
+    Width = 281
+    Height = 17
+    Caption = #1057#1082#1072#1095#1072#1090#1100' '#1092#1072#1081#1083#1099' '#1088#1077#1083#1080#1079#1072' '#1087#1086#1089#1083#1077' '#1076#1086#1073#1072#1074#1083#1077#1085#1080#1103' '#1087#1088#1086#1077#1082#1090#1072
+    TabOrder = 3
+  end
+  object mm: TMemo
+    Left = 16
+    Top = 408
+    Width = 553
+    Height = 89
+    TabOrder = 4
+  end
+  object Panel: TPanel
+    Left = 472
+    Top = 16
+    Width = 97
+    Height = 113
+    BevelOuter = bvLowered
+    BiDiMode = bdLeftToRight
+    ParentBiDiMode = False
+    TabOrder = 5
+    object ImagRepository: TImage
+      Left = 8
+      Top = 8
+      Width = 81
+      Height = 81
+      Stretch = True
+    end
+    object LblAvatar: TLabel
+      Left = 27
+      Top = 96
+      Width = 41
+      Height = 13
+      Caption = '(Avatar)'
+    end
+  end
+  object BtnApply: TButton
+    Left = 216
+    Top = 512
+    Width = 145
+    Height = 30
+    Caption = #1044' '#1054' '#1041' '#1040' '#1042' '#1048' '#1058' '#1068
+    TabOrder = 6
+    OnClick = BtnApplyClick
+  end
+  object edSaveDir: TEdit
+    Left = 16
+    Top = 104
+    Width = 417
+    Height = 21
+    MaxLength = 256
+    TabOrder = 7
+  end
+  object RGRulesNotis: TRadioGroup
+    Left = 344
+    Top = 176
+    Width = 225
+    Height = 121
+    Caption = #1055#1088#1072#1074#1080#1083#1072':'
+    ItemIndex = 0
+    Items.Strings = (
+      #1058#1086#1083#1100#1082#1086' '#1091#1074#1077#1076#1086#1084#1083#1103#1090#1100
+      #1059#1074#1077#1076#1086#1084#1083#1103#1090#1100' '#1080' '#1089#1082#1072#1095#1080#1074#1072#1090#1100
+      #1057#1082#1072#1095#1080#1074#1072#1090#1100' '#1073#1077#1079' '#1091#1074#1077#1076#1086#1084#1083#1077#1085#1080#1081)
+    TabOrder = 8
+  end
+  object RGRuleDownload: TRadioGroup
+    Left = 16
+    Top = 224
+    Width = 297
+    Height = 73
+    Caption = #1055#1088#1080#1074#1080#1083#1072' '#1089#1082#1072#1095#1080#1074#1072#1085#1080#1103' '#1092#1072#1081#1083#1086#1074
+    ItemIndex = 0
+    Items.Strings = (
+      #1057#1082#1072#1095#1080#1074#1072#1090#1100' '#1074#1089#1077' '#1092#1072#1081#1083#1099
+      #1057#1082#1072#1095#1080#1074#1072#1090#1100' '#1080#1089#1087#1086#1083#1100#1079#1091#1103' '#1092#1080#1083#1100#1090#1088)
+    TabOrder = 9
+  end
+  object GrBoxFilter: TGroupBox
+    Left = 16
+    Top = 312
+    Width = 553
+    Height = 81
+    Caption = #1060#1080#1083#1100#1090#1088' ('#1074#1087#1080#1096#1080#1090#1077' '#1089#1083#1086#1074#1072' '#1095#1077#1088#1077#1079' '#1079#1072#1087#1103#1090#1091#1102'):'
+    TabOrder = 10
+    object LblExclude: TLabel
+      Left = 272
+      Top = 28
+      Width = 61
+      Height = 13
+      Caption = #1048#1089#1082#1083#1102#1095#1080#1090#1100':'
+    end
+    object LblInclude: TLabel
+      Left = 8
+      Top = 28
+      Width = 55
+      Height = 13
+      Caption = #1042#1082#1083#1102#1095#1080#1090#1100':'
+    end
+    object EdFilterInclude: TEdit
+      Left = 8
+      Top = 44
+      Width = 249
+      Height = 21
+      TabOrder = 0
+      Text = 'windows, win, win64, 64bit'
+    end
+    object edFilterExclude: TEdit
+      Left = 272
+      Top = 44
+      Width = 265
+      Height = 21
+      TabOrder = 1
+      Text = 'mac, linux, 32bit'
+    end
+  end
+  object BtnClose: TButton
+    Left = 472
+    Top = 512
+    Width = 97
+    Height = 30
+    Caption = #1047#1040#1050#1056#1067#1058#1068
+    TabOrder = 11
+    OnClick = BtnCloseClick
+  end
+  object ChBoxAddVerToFileName: TCheckBox
+    Left = 16
+    Top = 168
+    Width = 249
+    Height = 17
+    Caption = #1055#1088#1080#1073#1072#1074#1083#1103#1090#1100' '#1082' '#1080#1084#1077#1085#1080' '#1092#1072#1081#1083#1072' '#1074#1077#1088#1089#1080#1102' '#1088#1077#1083#1080#1079#1072
+    TabOrder = 12
+  end
+end
---
+++ UFrmAddRepository.pas
@@ -0,0 +1,459 @@
+﻿unit UFrmAddRepository;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask,
+  Vcl.ComCtrls, System.ImageList, Vcl.ImgList, json, System.IOUtils,
+  REST.Types, RESTContentTypeStr, StrUtils,
+  Vcl.ExtCtrls, System.IniFiles, Vcl.Buttons, Vcl.FileCtrl;
+
+type TFrmShowMode = (fsmAddNew, fsmEdit);
+
+type
+  TFrmAddRepository = class(TForm)
+    edRepositoryLink: TEdit;
+    LblUrlProject: TLabel;
+    statTextProjectDir: TStaticText;
+    ChBoxSubDir: TCheckBox;
+    ChBoxDownloadLastRelease: TCheckBox;
+    mm: TMemo;
+    Panel: TPanel;
+    ImagRepository: TImage;
+    LblAvatar: TLabel;
+    BtnApply: TButton;
+    SpdBtnOpenDir: TSpeedButton;
+    edSaveDir: TEdit;
+    RGRulesNotis: TRadioGroup;
+    RGRuleDownload: TRadioGroup;
+    GrBoxFilter: TGroupBox;
+    EdFilterInclude: TEdit;
+    edFilterExclude: TEdit;
+    LblExclude: TLabel;
+    LblInclude: TLabel;
+    BtnClose: TButton;
+    ChBoxAddVerToFileName: TCheckBox;
+    procedure BtnApplyClick(Sender: TObject);
+    function ConverLinkToApiLink(Link: String): String;
+    procedure FrmShowInit;
+    function ExtractReposNameFromLink(URL: String): String;
+    function ExcludeTrailingURLDelimiter(URL: String): String;
+    function GetImageExtention(ContentType: string): String;
+    procedure edRepositoryLinkChange(Sender: TObject);
+    procedure SaveAddedNewProject(Index: Integer);
+    function CheckRepositoryExistes(URL: String): boolean;
+    procedure SpdBtnOpenDirClick(Sender: TObject);
+    procedure FormShowEdit(ReposIndex: integer);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure BtnCloseClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+    FApiProject         : string;
+    FApiReleases        : string;
+    FProjectName        : string;
+    FFullName           : string;
+    FAvatar_url         : string;
+    FProgectDir         : string;
+    FAvatarFileName     : string;
+    FLastReleaseVersion : string;
+    FProjChecked        : Boolean;
+    FPublishRelease     : string;
+    FLanguage           : string;
+    FReposIndex         : Integer;
+    FFrmMode            : TFrmShowMode;
+  public
+    { Public declarations }
+    Applay: Boolean;
+  end;
+
+var
+  FrmAddRepository: TFrmAddRepository;
+
+const
+   test_url = 'https://github.com/superbot-coder/chia_plotting_tools';
+  // 'http://api.github.com/repos/superbot-coder/chia_plotting_tools/releases'
+
+implementation
+
+{$R *.dfm}
+
+Uses UFrmMain;
+
+function TFrmAddRepository.CheckRepositoryExistes(URL: String): boolean;
+var i: SmallInt;
+begin
+  Result := False;
+  for i := 0 to Length(arReposList)-1 do
+  begin
+    if arReposList[i].ReposUrl = URL then
+    begin
+      Result := true;
+      exit;
+    end;
+  end;
+end;
+
+function TFrmAddRepository.ConverLinkToApiLink(Link: String): String;
+begin
+  if Link = '' then Exit;
+  if AnsiPos('https://', Link) > 0 then
+    Result := StringReplace(Link, 'https://github.com', 'https://api.github.com/repos', [rfIgnoreCase])
+  else
+    Result := StringReplace(Link, 'github.com', 'https://api.github.com/repos', [rfIgnoreCase]);
+  Result := Result;
+end;
+
+function TFrmAddRepository.ExcludeTrailingURLDelimiter(URL: String): String;
+var
+  len: Integer;
+begin
+  if URL = '' then Exit;
+  len := Length(URL);
+  Result := URL;
+  if (URL[len] = '/') or (URL[len] = '\')  then Delete(Result, len, 1);
+end;
+
+function TFrmAddRepository.ExtractReposNameFromLink(URL: String): String;
+var
+  x: Word;
+begin
+  Result := URL;
+  x := LastDelimiter('/', Result);
+  if x = 0 then
+  begin
+    Result := '';
+    Exit;
+  end;
+  Result :=  copy(Result, x + 1, Length(Result) - x);
+end;
+
+procedure TFrmAddRepository.FormCreate(Sender: TObject);
+begin
+  EdFilterInclude.MaxLength  := 500;
+  edFilterExclude.MaxLength  := 500;
+  edSaveDir.MaxLength        := 260;
+  edRepositoryLink.MaxLength := 500;
+end;
+
+procedure TFrmAddRepository.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = 27 then Close;
+end;
+
+procedure TFrmAddRepository.FormShowEdit(ReposIndex: integer);
+begin
+  FReposIndex := ReposIndex;
+  FFrmMode    := fsmEdit;
+  edRepositoryLink.Enabled := false;
+  edRepositoryLink.Text    := arReposList[ReposIndex].ReposUrl;
+  edSaveDir.Text           := arReposList[ReposIndex].ReposDir;
+  ChBoxSubDir.Checked      := arReposList[ReposIndex].NeedSubDir;
+  RGRuleDownload.ItemIndex := arReposList[ReposIndex].RuleDownload;
+  RGRulesNotis.ItemIndex   := arReposList[ReposIndex].RuleNotis;
+  EdFilterInclude.Text     := arReposList[ReposIndex].FilterInclude;
+  edFilterExclude.Text     := arReposList[ReposIndex].FilterExclude;
+  ChBoxDownloadLastRelease.Enabled := False;
+  BtnApply.Caption         := 'С О Х Р А Н И Т Ь';
+  if FileExists(arReposList[ReposIndex].AvatarFile) then
+    ImagRepository.Picture.LoadFromFile(arReposList[ReposIndex].AvatarFile);
+
+  ShowModal;
+end;
+
+procedure TFrmAddRepository.FrmShowInit;
+begin
+  // init controls
+  FFrmMode := fsmAddNew;
+  edRepositoryLink.Text    := '';
+  edRepositoryLink.Enabled := true;
+  edSaveDir.Text        := '';
+  BtnApply.Enabled      := False;
+  BtnApply.Caption      := 'Д О Б А В И Т Ь';
+  ImagRepository.Picture   := Nil;
+  RGRuleDownload.ItemIndex := 0;
+  RGRulesNotis.ItemIndex   := 0;
+  ChBoxSubDir.Checked      := false;
+  ChBoxAddVerToFileName.Checked    := false;
+  ChBoxDownloadLastRelease.Enabled := True;
+  EdFilterInclude.Text := 'windows, win, win64, 64bit';
+  edFilterExclude.Text := 'mac, linux, 32bit';
+
+  mm.Clear;
+
+  // init values
+  Applay              := false;
+  FApiProject         := '';
+  FProgectDir         := '';
+  FApiReleases        := '';
+  FProjectName        := '';
+  FFullName           := '';
+  FAvatar_url         := '';
+  FAvatarFileName     := '';
+  FLastReleaseVersion := '';
+
+  ShowModal;
+end;
+
+function TFrmAddRepository.GetImageExtention(ContentType: string): String;
+var
+  x: Byte;
+begin
+  x := AnsiIndexStr(ContentType, arContentTypeStr);
+  if x <> -1 then
+    case TRESTContentType(X) of
+      ctIMAGE_GIF     : Result := '.gif';
+      ctIMAGE_JPEG    : Result := '.jpeg';
+      ctIMAGE_PJPEG   : Result := '.pjpeg';
+      ctIMAGE_PNG     : Result := '.png';
+      ctIMAGE_SVG_XML : Result := '.svg';
+      ctIMAGE_TIFF    : Result := '.tiff';
+      ctIMAGE_X_XCF   : Result := '.xcf';
+    end
+    else Result := '';
+end;
+
+procedure TFrmAddRepository.SaveAddedNewProject(Index: Integer);
+var
+  INI: TIniFile;
+  Section: string;
+begin
+
+  if Not DirectoryExists(ConfigDir) then ForceDirectories(ConfigDir);
+  Section := 'PROJECT_LIST\'+ StringReplace(arReposList[Index].FullReposName, '/', '_', [rfReplaceAll]);
+
+  INI := TIniFile.Create(FileConfig);
+  try
+    with INI do
+    begin
+      WriteString(Section, 'ProjectUrl', arReposList[Index].ReposUrl);
+      WriteString(Section, 'ProjectDir', arReposList[Index].ReposDir);
+      WriteString(Section, 'ApiProjUrl', arReposList[Index].ApiReposUrl);
+      WriteString(Section, 'ApiReleasesUrl', arReposList[Index].ApiReleasesUrl);
+      WriteString(Section, 'ProjectName', arReposList[Index].ReposName);
+      WriteString(Section, 'FullProjectName', arReposList[Index].FullReposName);
+      WriteString(Section, 'AvatarFile', arReposList[Index].AvatarFile);
+      WriteString(Section, 'AvatarUrl',arReposList[Index].AvatarUrl );
+      WriteString(Section, 'FilterInclude', arReposList[Index].FilterInclude);
+      WriteString(Section, 'FilterExclude', arReposList[Index].FilterExclude);
+      WriteString(Section, 'DatePublish', arReposList[Index].DatePublish);
+      WriteString(Section, 'Language', arReposList[Index].Language);
+      WriteString(Section, 'LastVersion', arReposList[Index].LastVersion);
+      WriteDateTime(Section, 'LastChecked', arReposList[Index].LastChecked);
+      WriteInteger(Section, 'RuleDownload', arReposList[Index].RuleDownload);
+      WriteInteger(Section, 'RuleNotis', arReposList[Index].RuleNotis);
+      WriteBool(Section, 'NeedSubDir', arReposList[Index].NeedSubDir);
+      WriteDateTime(Section, 'NewReleaseDT', arReposList[Index].NewReleaseDT);
+      WriteBool(Section, 'AddVerToFileName', arReposList[Index].AddVerToFileName);
+    end;
+  finally
+    INI.Free;
+  end;
+end;
+
+procedure TFrmAddRepository.SpdBtnOpenDirClick(Sender: TObject);
+var
+  SelDir: String;
+begin
+  SelectDirectory('Выберите каталог', '', SelDir);
+  edSaveDir.Text := SelDir;
+end;
+
+procedure TFrmAddRepository.BtnApplyClick(Sender: TObject);
+var
+  JSONData : TJSONValue;
+  ext      : string;
+  x        : SmallInt;
+begin
+
+  if FFrmMode = fsmEdit then
+  begin
+    with arReposList[FReposIndex] do
+    begin
+      ReposDir      := edSaveDir.Text;
+      NeedSubDir    := ChBoxSubDir.Checked;
+      RuleDownload  := RGRuleDownload.ItemIndex;
+      RuleNotis     := RGRulesNotis.ItemIndex;
+      FilterInclude := EdFilterInclude.Text;
+      FilterExclude := edFilterExclude.Text;
+      AddVerToFileName := ChBoxAddVerToFileName.Checked;
+    end;
+    SaveAddedNewProject(FReposIndex);
+  end;
+
+  if FFrmMode = fsmAddNew then
+  begin
+    BtnApply.Enabled := false;
+    if edRepositoryLink.Text = '' then edRepositoryLink.Text := test_url;
+
+    if edRepositoryLink.Text = '' then
+    begin
+      MessageBox(Handle, PChar('Введите ссылку на проект'),
+               PChar(CAPTION_MB), MB_ICONWARNING);
+      Exit;
+    end;
+
+    if Not AnsiContainsStr(AnsiLowerCase(edRepositoryLink.Text), 'https://') then
+      edRepositoryLink.Text := 'https://' + edRepositoryLink.Text;
+
+    // Проверяю что проект уже добавлен в список
+    // Checking that the project has already been added to the list
+    if CheckRepositoryExistes(edRepositoryLink.Text) then
+    begin
+      MessageBox(Handle, PChar('Такой проек уже добавлен в список..'),
+                 PChar(CAPTION_MB), MB_ICONWARNING);
+      Exit;
+    end;
+
+    edRepositoryLink.Text := ExcludeTrailingURLDelimiter(Trim(edRepositoryLink.Text));
+    FApiProject         := ConverLinkToApiLink(edRepositoryLink.Text);
+    FApiReleases        := FApiProject + '/releases';
+
+    with FrmMain do
+    begin
+      // проверка правильная ссылки; Check Existes the project
+      RESTResponse.RootElement := '';
+      RESTClient.Accept        := arContentTypeStr[ord(ctAPPLICATION_JSON)];
+      RESTClient.BaseURL       := FApiProject;
+      RESTRequest.Execute;
+
+      if RESTResponse.StatusCode <> 200 then
+      begin
+        MessageBox(Handle, PChar('Ошибка: ' + RESTResponse.StatusText +
+                   ' код: ' + IntToStr(RESTResponse.StatusCode)),
+                   PChar(CAPTION_MB), MB_ICONERROR);
+        exit;
+      end;
+
+      if RESTResponse.JSONValue.FindValue('name') = Nil then
+      begin
+        //
+        Exit;
+      end;
+      // Получаю имя проекта; Getting name the project
+      FProjectName := RESTResponse.JSONValue.FindValue('name').Value;
+
+      if RESTResponse.JSONValue.FindValue('full_name') = Nil then
+      begin
+        //
+        Exit;
+      end;
+      // Получаю полное имя проекта; Getting full name the project
+      FFullName := RESTResponse.JSONValue.FindValue('full_name').Value;
+      // Заменяю символ "/" на "_" ;
+      mm.Lines.Add('Название проекта: ' + FProjectName);
+
+      // Получаю URL аватарки проекта; Getting the avatar URL of the project
+      JSONData := RESTResponse.JSONValue.FindValue('owner').FindValue('avatar_url');
+      if JSONData <> Nil then FAvatar_url := JSONData.Value;
+
+      // получаю язык программирования проекта; getting the project programming language
+      FLanguage := RESTResponse.JSONValue.FindValue('language').Value;
+
+      // Получаю директорию проекта; Getting the project directory
+      if edSaveDir.Text <> '' then
+        FProgectDir := edSaveDir.Text
+      else
+        FProgectDir := GLProjectsDir + PathDelim +
+                       StringReplace(FFullName, '/', '_', [rfReplaceAll, rfIgnoreCase]);
+      if Not DirectoryExists(FProgectDir) then ForceDirectories(FProgectDir);
+
+      // Скачиваю файл аватарки; Downloading avatarka file
+      RESTClient.BaseURL       := FAvatar_url;
+      RESTClient.Accept        := '';
+      RESTResponse.RootElement := '';
+      RESTRequest.Execute;
+
+      if RESTResponse.StatusCode = 200 then // 'StatusCode = 200 OK.
+      begin
+        // получаю расширение и тип картинки; getting extension and image(avatar) type
+        ext := GetImageExtention(RESTResponse.ContentType);
+        // Пoлучаю имя файла для сохранения аватарки
+        // Getting the file name to save the avatar
+        FAvatarFileName := FProgectDir + '\Avatar' + ext;
+        TFile.WriteAllBytes(FAvatarFileName, RESTResponse.RawBytes);
+        ImagRepository.Picture.LoadFromFile(FAvatarFileName);
+      end;
+
+      // Проверяю существуют ли релизы; Checking if releases exist
+      // Поучаю самую последнюю версию релиза; I'll get the latest release
+      RESTClient.BaseURL       := FApiReleases;
+      RESTClient.Accept        := arContentTypeStr[ord(ctAPPLICATION_JSON)];
+      RESTResponse.RootElement := '[0]';
+      RESTRequest.Execute;
+
+      If RESTResponse.StatusCode <> 200 then
+      begin
+        mm.Lines.Add('Ошибка: StatusCode: ' + IntToStr(RESTResponse.StatusCode) +
+                     ' ' + RESTResponse.StatusText);
+        Exit;
+      end;
+
+      if RESTResponse.JSONValue = Nil then
+      begin
+        mm.Lines.Add('Не найдено ни одного опубликованого релиза.');
+        mm.Lines.Add('Проверка выполнена.');
+        MessageBox(Handle, PChar(mm.Lines.Text),
+                PChar(CAPTION_MB), MB_ICONINFORMATION);
+      end;
+
+      if RESTResponse.JSONValue.FindValue('tag_name') <> nil then
+      begin
+        mm.Lines.Add('Имя релиза: ' + FProjectName);
+        FLastReleaseVersion := RESTResponse.JSONValue.FindValue('tag_name').Value;
+        FPublishRelease     := RESTResponse.JSONValue.FindValue('published_at').Value;
+        FPublishRelease     := ConvertGitHubDateToDateTime(FPublishRelease);
+        mm.Lines.Add('Дата публикации последнего релиза: ' + FPublishRelease);
+        mm.Lines.Add('Версия последнего релиза: ' + FLastReleaseVersion);
+        mm.Lines.Add('Проверка выполнена.');
+        MessageBox(Handle, PChar(mm.Lines.Text), PChar(CAPTION_MB), MB_ICONINFORMATION);
+      end;
+    end;
+
+    // Добавляю новую запись в массив arReposList
+    // Adding a new entry to the array arReposList
+    SetLength(arReposList, Length(arReposList) + 1);
+    with arReposList[Length(arReposList) - 1] do
+    begin
+      ReposUrl         := edRepositoryLink.Text;
+      ReposDir         := FProgectDir;
+      ApiReposUrl      := FApiProject;
+      ApiReleasesUrl   := FApiReleases;
+      ReposName        := FProjectName;
+      FullReposName    := FFullName;
+      AvatarFile       := FAvatarFileName;
+      AvatarUrl        := FAvatar_url;
+      FilterInclude    := EdFilterInclude.Text;
+      FilterExclude    := edFilterExclude.Text;
+      DatePublish      := FPublishRelease;
+      Language         := FLanguage;
+      LastVersion      := FLastReleaseVersion;
+      LastChecked      := Date + Time;
+      RuleDownload     := RGRuleDownload.ItemIndex;
+      RuleNotis        := RGRulesNotis.ItemIndex;
+      NewReleaseDT     := Date + Time;
+      AddVerToFileName := ChBoxAddVerToFileName.Checked;
+    end;
+
+    SaveAddedNewProject(Length(arReposList) - 1);
+
+    Applay := true;
+    BtnApply.Enabled := true;
+  end;
+
+  Close;
+end;
+
+procedure TFrmAddRepository.BtnCloseClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TFrmAddRepository.edRepositoryLinkChange(Sender: TObject);
+begin
+  BtnApply.Enabled := true;
+end;
+
+end.
